5fecf7e89ab90247463a2788c5fd7d82e4d59f3f
kimgeuntae <kok617@naver.com>
2019-12-01 00:25:47 +0900

    Add parsed movies[] from YTS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import React from 'react';
+import React from "react";
 import axios from "axios";
-
-var func = x => x*2;
-
-console.log(func(3));
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
@@ -12,7 +9,14 @@ class App extends React.Component {
   };
 
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
   };
 
   componentDidMount() {
@@ -20,15 +24,24 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
       <div>
-        {isLoading ? "Loading..." : "We are ready" }
+        {isLoading
+          ? "Loading..."
+          : movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
       </div>
-    )
+    );
   }
-
 }
-
 
 export default App;
