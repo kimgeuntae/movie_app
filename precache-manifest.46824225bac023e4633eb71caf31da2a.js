b1f89860345d2d460b056758f31017067e849756
kimgeuntae <kok617@naver.com>
2019-12-02 17:26:54 +0900

    Updates

---
+++ precache-manifest.46824225bac023e4633eb71caf31da2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde359a79aaa9a9e53ef565175e286ca",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "6fb616922f41086f1401",
+    "url": "/movie_app/static/css/main.1a5d9380.chunk.css"
+  },
+  {
+    "revision": "6f0599dff9eecd2a0e73",
+    "url": "/movie_app/static/js/2.43e6c5f9.chunk.js"
+  },
+  {
+    "revision": "6fb616922f41086f1401",
+    "url": "/movie_app/static/js/main.5091dbe1.chunk.js"
+  },
+  {
+    "revision": "7259bafd2195d7f70cbd",
+    "url": "/movie_app/static/js/runtime-main.c454ae6d.js"
+  }
+]);
