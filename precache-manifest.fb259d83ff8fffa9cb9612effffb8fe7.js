70b0c79f9b3dfd1afb3b978470ff45c6ca8a89c6
kimgeuntae <kok617@naver.com>
2019-12-03 14:13:50 +0900

    Updates

--- precache-manifest.fb259d83ff8fffa9cb9612effffb8fe7.js
+++ precache-manifest.fb259d83ff8fffa9cb9612effffb8fe7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7abfa8be3454dfda53234be10795962b",
+    "revision": "9fb61469969b907e2b8ad8e208b3239b",
     "url": "/movie_app/index.html"
   },
   {
-    "revision": "6fb616922f41086f1401",
-    "url": "/movie_app/static/css/main.1a5d9380.chunk.css"
+    "revision": "8934fafabe5baafed1a7",
+    "url": "/movie_app/static/css/main.c60e4a87.chunk.css"
   },
   {
     "revision": "6f0599dff9eecd2a0e73",
     "url": "/movie_app/static/js/2.43e6c5f9.chunk.js"
   },
   {
-    "revision": "6fb616922f41086f1401",
-    "url": "/movie_app/static/js/main.5091dbe1.chunk.js"
+    "revision": "8934fafabe5baafed1a7",
+    "url": "/movie_app/static/js/main.41867556.chunk.js"
   },
   {
     "revision": "7259bafd2195d7f70cbd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.e5b634477235230c21f603d3e0170d65.js"
+  "/movie_app/precache-manifest.fb259d83ff8fffa9cb9612effffb8fe7.js"
 );
 
 self.addEventListener('message', (event) => {
